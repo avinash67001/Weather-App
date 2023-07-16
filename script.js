dd6b9013e605cafca20986f9e382a641ea4b6315
avinash67001 <111963672+avinash67001@users.noreply.github.com>
2023-07-16 16:22:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let weather = {
+    apikey: "a6cdd6c630f638cb772c046d3564c89f",
+    fetchweather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apikey
+        ).then((response) => response.json())
+            .then((data) => {
+                this.displayWeather(data);
+            });
+    },
+    displayWeather: function (data) {
+        // This would fetch the relative argument from the object and would convert that into a variable
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        // console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity : " + humidity +"%";
+        document.querySelector(".wind").innerText = "Wind Speed : " +  speed + " Km/Hr";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name +"')"
+    },
+    search : function(){
+        this.fetchweather(document.querySelector(".search-box").value); 
+    }
+
+};
+
+document.querySelector(".search button").addEventListener("click",function (){
+    weather.search();
+});
+
+document.querySelector(".search-box").addEventListener("keyup",function (event){
+    if(event.key == "Enter"){
+    weather.search();
+    }
+});
+// On first Load
+weather.fetchweather("Delhi"); 
